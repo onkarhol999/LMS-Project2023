6c42aa8cfb86808df90cd6ecfe7cb2e8c992b250
Onkar hol <holonkar2003@gmail.com>
2023-12-04 13:17:22 +0530

    added basic layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,20 @@
 
 import './App.css';
 
-import { Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
-import Footer from './Components/footer';
+import HomePage from './Pages/HomePage'
+
+// import Footer from './Components/footer';
 
 function App() {
 
   return (
     <>
    
-      {/* <Routes> */}
-
-
-      {/* </Routes> */}
-      <Footer/>
+      <Routes>
+          <Route path='/' element={<HomePage/>}></Route>
+      </Routes>
     </>
   )
 }
