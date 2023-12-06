182e007d3442ac77073b631e0ce778109badda7b
Onkar hol <holonkar2003@gmail.com>
2023-12-06 06:52:28 +0530

    Prepared course deatails componentas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import { Route, Routes } from 'react-router-dom';
 
 import AboutUs from './Pages/AboutUs';
 import Contact from './Pages/Contact';
+import CourseDescription from './Pages/Course/CourseDescription';
 import CourseList from './Pages/Course/CourseList';
 import Denied from './Pages/Denied';
 import HomePage from './Pages/HomePage'
@@ -24,6 +25,8 @@ function App() {
           <Route path='/about' element={<AboutUs/>}></Route>
           <Route path='/signup' element={<Signup/>}></Route>
           <Route path='/contact' element={<Contact/>}></Route>
+          <Route path='/course/description/' element={<CourseDescription/>}></Route>
+
           <Route path='/denied' element={<Denied/>}></Route>
           <Route path='/courses' element={<CourseList/>}></Route>
           <Route path='/login' element={<Login/>}></Route>
